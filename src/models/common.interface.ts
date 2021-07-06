30f5e61b1f593d7df79888b905dbf3ce407a2fe0
Aiswarya <aiswaryav@qburst.com>
2021-07-06 16:30:55 +0530

    Added common collective for typescript interfaces

--- src/models/common.interface.ts
+++ src/models/common.interface.ts
@@ -7,6 +7,36 @@ export interface Field {
     name?: string;
     label: string;
     type: string;
-    options?: Array<Option>;
+    options?: Option[];
     required?: boolean;
+    disabled?: boolean;
+    value?: string;
+    handleFieldValueChange?: (e: React.SyntheticEvent) => void;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    salary: number;
+}
+
+export interface Slab {
+    start: number;
+    end?: number;
+    rate: number;
+}
+
+export interface TotalTax {
+    totalIncome?: number;
+    totalDeduction?: number;
+    netTaxIncome: number;
+    annual: number;
+}
+
+export interface Data {
+    [key: string]: Field[];
+}
+
+export interface Amount {
+    [key: string]: number;
 }
