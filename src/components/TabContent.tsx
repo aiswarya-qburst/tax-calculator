e94e7ab87bdaf8347835505268ad05ace36593fe
Aiswarya <aiswaryav@qburst.com>
2021-07-07 16:04:22 +0530

    Use reusable component for income and deduction

--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,35 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react';
-import Deduction from './Deduction';
-import Income from './Income';
+import GenerateForm from './GenerateForm';
+// import Income from './Income';
 import Slabs from './Slabs';
 import Total from './Total';
-import { getTotalTax, getTotalIncome } from './taxEngine/taxCalculator';
 import { BasicContext } from '../App';
-import { TotalTax } from '../models/common.interface';
 
 const TabContent = ({ active }: { active: string }): JSX.Element => {
     const [income, updateIncome] = useState(0);
     const [deduction, updateDeduction] = useState(0);
 
-    const { slab, total, updateTotal } = useContext(BasicContext);
+    const { totalTax, updateTotal } = useContext(BasicContext);
 
     useEffect(() => {
-        const total: TotalTax = {
-            totalIncome: income,
-            totalDeduction: deduction,
-            netTaxIncome: getTotalIncome(income, deduction),
-            annual: getTotalTax(getTotalIncome(income, deduction), slab),
-        };
-
-        updateTotal(total);
+        updateTotal({ income, deduction });
     }, [income, deduction]);
 
     return (
         <div>
             {active === 'tax-slab' && <Slabs />}
-            {active === 'income' && <Income handleIncomeUpdate={updateIncome} />}
-            {active === 'deduction' && <Deduction />}
-            {active === 'total' && <Total total={total} />}
+            {active === 'income' && <GenerateForm formType="income" handleSectionTotalUpdate={updateIncome} />}
+            {active === 'deduction' && <GenerateForm formType="deduction" handleSectionTotalUpdate={updateDeduction} />}
+            {active === 'total' && <Total total={totalTax} />}
         </div>
     );
 };
