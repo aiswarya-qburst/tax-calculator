e4c7789b1439271e194217661063422b2b42634f
Aiswarya <aiswaryav@qburst.com>
2021-07-08 12:34:40 +0530

    Persist form data in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, Suspense, useEffect, useState } from 'react';
 import Basic from './components/Basic';
 import Header from './components/Header';
-import Sections from './components/Sections';
-import { Slab, TotalTax, User } from './models/common.interface';
+// import Sections from './components/Sections';
+import { getTotalTax, getTotalIncome, getWithCess } from './components/taxEngine/taxCalculator';
+import { IncOrDed, Slab, TotalTax, User } from './models/common.interface';
+const Sections = React.lazy(() => import('./components/Sections'));
 
 export const BasicContext = createContext(null);
 
@@ -11,27 +13,51 @@ const App: React.FunctionComponent = () => {
     const [slab, updateSlab] = useState([] as Array<Slab>);
     const [empData, updateEmpData] = useState({} as User);
     const [ay, updateAY] = useState('');
-    const [total, updateTotal] = useState({} as TotalTax);
+    const [total, updateTotal] = useState({} as IncOrDed);
+    const [totalTax, updateTotalTax] = useState({} as TotalTax);
 
     const contextValue = {
         slab,
         regime,
         empData,
         ay,
-        total,
+        totalTax,
         updateTotal,
         updateSlab,
     };
 
+    useEffect(() => {
+        const totalIncome = getTotalIncome(total.income, total.deduction);
+        const totalTax = getTotalTax(totalIncome, slab);
+        const totalTaxWithCess = getWithCess(totalTax.annual, 4);
+
+        const totalTaxAmount: TotalTax = {
+            totalIncome: total.income,
+            totalDeduction: total.deduction,
+            netTaxIncome: totalIncome,
+            taxWithDescription: totalTax,
+            cess: 4,
+            withCess: totalTaxWithCess,
+        };
+
+        updateTotalTax(totalTaxAmount);
+    }, [regime, slab, total]);
+
+    useEffect(() => {
+        return () => {
+            localStorage.clear();
+        };
+    });
+
     return (
         <div>
-            <Header tax={total} />
+            <Header tax={Object.keys(totalTax).length ? totalTax.withCess : 0} />
             <Basic updateRegime={updateRegime} updateEmpData={updateEmpData} updateAY={updateAY} />
-            {Object.keys(empData).length > 0 && ay.length > 0 && regime.length > 0 && (
-                <BasicContext.Provider value={contextValue}>
+            <BasicContext.Provider value={contextValue}>
+                <Suspense fallback={<div>Loading...</div>}>
                     <Sections />
-                </BasicContext.Provider>
-            )}
+                </Suspense>
+            </BasicContext.Provider>
         </div>
     );
 };
--- src/components/GenerateForm.tsx
+++ src/components/GenerateForm.tsx
@@ -6,6 +6,7 @@ import { BasicContext } from '../App';
 import { Initial, Field } from '../models/common.interface';
 import { getTotal } from './taxEngine/taxCalculator';
 import { getName } from './utils';
+import { useCallback } from 'react';
 
 const GenerateForm = ({
     formType,
@@ -16,28 +17,35 @@ const GenerateForm = ({
 }): JSX.Element => {
     const [form, updateForm] = useState([] as Field[]);
     const [initial, setInitial] = useState({} as Initial);
-    const { ay, empData } = useContext(BasicContext);
+    const { regime, ay, empData } = useContext(BasicContext);
     const { result, loading } = useFetch(formType);
 
     const handleSubmit = (fieldData: Record<string, string>) => {
+        localStorage.setItem(formType, JSON.stringify(fieldData));
         const total: number = getTotal(Object.values(fieldData).map(Number));
         handleSectionTotalUpdate(total);
     };
 
-    useEffect(() => {
-        !loading && result && result[ay] && updateForm(result[ay][formType]);
-    }, [result, loading]);
+    const getSavedForm = useCallback(() => JSON.parse(localStorage.getItem(formType)), [formType]);
 
     useEffect(() => {
+        !loading && result && result[ay] && updateForm(result[ay][formType]);
+    }, [regime, ay, result, loading]);
+
+    useEffect(() => {
+        const savedForm = getSavedForm();
         let val = {};
 
         form &&
             form.map((f) => {
-                val = { ...val, [getName(f.label)]: f.label === 'Salary' ? empData.salary : '' };
+                const name = getName(f.label);
+                const fieldValue = !savedForm ? '' : savedForm[name];
+
+                val = { ...val, [name]: f.label === 'Salary' ? empData.salary : fieldValue };
             });
 
         setInitial(val);
-    }, [form]);
+    }, [form, empData.salary]);
 
     return ay.length === 0 || empData.length === 0 ? (
         <p className="text-teal-700 mt-4 flex justify-center">Select a Regime and Assessment year</p>
