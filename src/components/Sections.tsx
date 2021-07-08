070644d03139a0ca2814ec5d974801ce2c1a51ae
Aiswarya <aiswaryav@qburst.com>
2021-07-08 12:37:08 +0530

    Switch to tax slab tab when regime is changed

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,11 +1,18 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import TabContent from './TabContent';
 import Tabs from './Tabs';
+import { BasicContext } from '../App';
+import { useEffect } from 'react';
 
 const tabs = ['tax-slab', 'income', 'deduction', 'total'];
 
 const Sections: FC = () => {
     const [active, setActive] = useState('tax-slab');
+    const { regime } = useContext(BasicContext);
+
+    useEffect(() => {
+        setActive('tax-slab');
+    }, [regime]);
 
     return (
         <div className="mt-8">
