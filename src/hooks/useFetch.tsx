b870f94d699cd1f72c61998478c315871f0bc7d5
Aiswarya <aiswaryav@qburst.com>
2021-07-09 12:30:56 +0530

    Use proper return type

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
+import { Field } from '../models/formelement.interface';
+import { Slab } from '../models/slab.interface';
+import { User } from '../models/user.interface';
 
-const useFetch = (param: string): { result: Array<any>; loading: boolean } => {
+const useFetch = (
+    param: string,
+): { result: User[] | Record<string, Record<string, Field[]>> | Record<string, Slab[]>; loading: boolean } => {
     const [result, setResult] = useState([]);
     const [loading, setLoading] = useState(true);
 
