9cab07423f035de82642c126423475f6ea69c710
Aiswarya <aiswaryav@qburst.com>
2021-07-09 12:31:46 +0530

    Added test for all calculation algorithms

--- src/components/taxEngine/tests/calc.test.ts
+++ src/components/taxEngine/tests/calc.test.ts
@@ -1,95 +1,72 @@
 import { getTotalTax, getWithCess, getTotal, getTotalIncome } from '../taxCalculator';
 import 'isomorphic-fetch';
 
-async function getSlab(regime) {
+async function getSlab(regime: string) {
     return fetch(`http://localhost:8000/tax-slab`)
         .then((result) => result.json())
         .then((data) => data[regime])
         .catch((e) => console.error(e));
 }
 
+const income = [1650000, 20000, 5000];
+
+const deduction = [150000, 12000, 8000];
+
+const cess = 4;
+
 const oldRegimetestSet = [
-    [20000, 0],
-    [100000, 0],
-    [300000, 2500],
-    [600000, 32500],
-    [872000, 86900],
-    [1128303, 150991],
-    [1445000, 246000], // 2,55,840 - 9,840 = 246000
-    [1499999, 262500],
-    [1500000, 262500],
-    [2054385, 428816]
+    [20000, 0, 0],
+    [100000, 0, 0],
+    [300000, 2500, 2600],
+    [600000, 32500, 33800],
+    [872000, 86900, 90376],
+    [1128303, 150991, 157030.64],
+    [1445000, 246000, 255840], // 2,55,840 - 9,840 = 246000
+    [1499999, 262500, 273000],
+    [1500000, 262500, 273000],
+    [2054385, 428816, 445968.64]
 ];
 
 const newRegimetestSet = [
-    // [22000, 2000, 20000, 0, 0],
-    [100000, 0],
-    [300000, 2500],
-    [600000, 22500],
-    [872000, 55800],
-    [1128303, 100661],
-    [1445000, 173750],
-    [1499999, 187500],
-    [1500000, 187500],
-    [2054385, 353816],
-    [1908000, 309900] // 3,22,296 - 12,396 = 309900
+    [20000, 0, 0],
+    [100000, 0, 0],
+    [300000, 2500, 2600],
+    [600000, 22500, 23400],
+    [872000, 55800, 58032],
+    [1128303, 100661, 104687.44],
+    [1445000, 173750, 180700],
+    [1499999, 187500, 195000],
+    [1500000, 187500, 195000],
+    [2054385, 353816, 367968.64],
+    [1908000, 309900, 322296] // 3,22,296 - 12,396 = 309900
 ];
 
+describe("Total of amounts", () => {
+    test("Income", () => {
+        expect(getTotal(income)).toBe(1675000);
+    });
+    test("Deductions", () => {
+        expect(getTotal(deduction)).toBe(170000);
+    });
+    test("Total Income - Total Deduction", () => {
+        expect(getTotalIncome(getTotal(income), getTotal(deduction))).toBe(1505000);
+    });
+});
+
 describe("Tax according to Old Regime", () => {
-    test.each(oldRegimetestSet)("Annual tax for income of Rs %i", (a, expected) => {
+    test.each(oldRegimetestSet)("Annual tax for income of Rs %i", (netIncome, total, totalWithCess) => {
         getSlab("old")
-            .then((slab) => expect(Math.round(getTotalTax(a, slab))).toBe(expected))
+            .then((slab) => expect(Math.round(getTotalTax(netIncome, slab).annual)).toBe(total))
+            .then(() => expect(getWithCess(total, cess)).toBe(totalWithCess))
             .catch((e) => console.error(e));
     });
 });
 
 describe("Tax according to New Regime", () => {
-    test.each(newRegimetestSet)("Annual tax for income of Rs %i", (a, expected) => {
+    test.each(newRegimetestSet)("Annual tax for income of Rs %i", (netIncome, total, totalWithCess) => {
         getSlab("new")
-            .then((slab) => expect(Math.round(getTotalTax(a, slab))).toBe(expected))
+            .then((slab) => expect(Math.round(getTotalTax(netIncome, slab).annual)).toBe(total))
+            .then(() => expect(getWithCess(total, cess)).toBe(totalWithCess))
             .catch((e) => console.error(e));
     });
 });
-
-// const calc = (netTaxIncome, slab) => {
-//     let annual = 0;
-//     let result = [{ range: 'Gross income', rate: netTaxIncome, final: '-' }];
-
-//     slab.every((s) => {
-//         //if rate is 0, can return 0. No calculation necessary
-//         if (!s.end || (netTaxIncome > s.start && netTaxIncome <= s.end)) {
-//             annual = annual + (netTaxIncome - s.start) * (s.rate / 100);
-//             result = [
-//                 ...result,
-//                 {
-//                     range: `${s.start} - ${s.end}`,
-//                     rate: `${s.rate}% [${s.rate}% of (Rs ${netTaxIncome} - Rs ${s.start})]`,
-//                     final: `${(netTaxIncome - s.start) * (s.rate / 100)}`,
-//                 },
-//             ];
-//             return false;
-//         } else if (netTaxIncome > s.end) {
-//             annual = annual + (s.end - s.start) * (s.rate / 100);
-//             result = [
-//                 ...result,
-//                 {
-//                     range: `${s.start} - ${s.end}`,
-//                     rate: `${s.rate}% [${s.rate}% of (Rs ${s.end} - Rs ${s.start})]`,
-//                     final: `${(s.end - s.start) * (s.rate / 100)}`,
-//                 },
-//             ];
-//         }
-
-//         return true;
-//     });
-//     console.table([
-//         ...result,
-//         {
-//             range: '-',
-//             rate: 'Total:',
-//             final: Math.round(annual),
-//         },
-//     ]);
-
-//     return Math.round(annual);
-// };
