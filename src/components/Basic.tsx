36ae813307ba83021c9c3657ffd307e71706e59b
Aiswarya <aiswaryav@qburst.com>
2021-07-14 09:16:01 +0530

    Remove unnecessary fields

--- src/components/Basic.tsx
+++ src/components/Basic.tsx
@@ -1,84 +1,50 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Form } from './FormElements';
 import InputForm from './InputForm';
 import { Field } from '../models/iFormElement';
-import { User } from '../models/iUser';
 
-const general: Field[] = [
-    {
-        type: 'text',
-        label: 'Employee ID',
-    },
-    {
-        type: 'dropdown',
-        label: 'Regime',
-        required: true,
-        options: [
-            {
-                label: 'Old Regime',
-                value: 'old',
-            },
-            {
-                label: 'New Regime',
-                value: 'new',
-            },
-        ],
-    },
-    {
-        type: 'dropdown',
-        label: 'Assessment year',
-        required: true,
-        options: [
-            {
-                label: '2020-21',
-                value: '20-21',
-            },
-            {
-                label: '2021-22',
-                value: '21-22',
-            },
-        ],
-    },
-];
+const regime: Field = {
+    type: 'dropdown',
+    label: 'Regime',
+    name: 'regime',
+    required: true,
+    options: [
+        {
+            label: 'Old Regime',
+            value: 'old',
+        },
+        {
+            label: 'New Regime',
+            value: 'new',
+        },
+    ],
+};
 
 //: React.FunctionComponent
-const Basic = ({
-    updateRegime,
-    updateEmpData,
-    updateAY,
-}: {
-    updateRegime: (r: string) => void;
-    updateEmpData: (e: User) => void;
-    updateAY: (a: string) => void;
-}): JSX.Element => {
-    const setBasicDetails = useCallback((empid: number) => {
-        fetch(`http://localhost:8000/users/${empid}`)
-            .then((res) => res.json())
-            .then((empData) => {
-                //TODO: Should send whole empData?
-                updateEmpData(empData);
-            });
-    }, []);
+const Basic = ({ updateRegime }: { updateRegime: (r: string) => void }): JSX.Element => {
+    // const setBasicDetails = useCallback((empid: number) => {
+    //     fetch(`http://localhost:8000/users/${empid}`)
+    //         .then((res) => res.json())
+    //         .then((empData) => {
+    //             //TODO: Should send whole empData?
+    //             updateEmpData(empData);
+    //         });
+    // }, []);
 
     const handleChange = (e) => {
         //TODO: Dont hardcode
-        if (e.target.name === 'employee_id') {
-            setBasicDetails(Number(e.target.value));
-        }
         if (e.target.name === 'regime') {
             updateRegime(e.target.value);
         }
-        if (e.target.name === 'assessment_year') {
-            updateAY(e.target.value);
-        }
+        // if (e.target.name === 'assessment_year') {
+        //     updateAY(e.target.value);
+        // }
     };
 
     return (
         <div className="basic-section">
-            <Form initialValues={{ initial: general }}>
-                {general.map((g) => (
-                    <InputForm key={g.label} field={g} updateDetails={handleChange} />
-                ))}
+            <Form initialValues={{ regime }}>
+                <InputForm field={regime} updateDetails={handleChange} />
             </Form>
         </div>
     );
