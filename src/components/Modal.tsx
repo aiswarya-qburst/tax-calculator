bb04997a5882c37bc48e3ba4fdc3f8f7675bdb84
Aiswarya <aiswaryav@qburst.com>
2021-07-14 09:13:08 +0530

    Modal to enter new field data

---
+++ src/components/Modal.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Field } from '../models/iFormElement';
+import { Form, FormSubmitButton, UtilityButton } from './FormElements';
+import InputForm from './InputForm';
+
+const formData: Field[] = [
+    {
+        label: 'Type of income',
+        type: 'text',
+        name: 'income_type',
+    },
+];
+
+const Modal = ({
+    show,
+    handleShow,
+    setNewField,
+}: {
+    show: boolean;
+    handleShow: (b: boolean) => void;
+    setNewField: (f: Record<string, string>) => void;
+}): JSX.Element => {
+    const handleSubmit = (data: Record<string, string>) => {
+        console.warn(data);
+
+        const newField = { [data.income_type]: '' };
+        setNewField(newField);
+        handleShow(false);
+    };
+
+    return (
+        show && (
+            <div className="modal">
+                <Form initialValues={{ initial: formData }} handleSubmit={handleSubmit}>
+                    {formData.map((f) => (
+                        <InputForm key={f.name} field={f} />
+                    ))}
+                    <FormSubmitButton title="Add" />
+                    <UtilityButton title="Close" handleClick={() => handleShow(false)} />
+                </Form>
+            </div>
+        )
+    );
+};
+
+export default Modal;
